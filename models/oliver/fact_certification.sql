6dc1cceb78018aa3c4c050a9001f55a337d8d600
Blake Hobbs <A02378587@usu.edu>
2026-03-21 00:21:53 +0000

    Core #6 Errors

--- models/oliver/fact_certification.sql
+++ models/oliver/fact_certification.sql
@@ -1,11 +1,29 @@
+{{ config(
+    materialized = 'table',
+    schema = 'dw_oliver'
+) }}
+
 SELECT
     e.employee_key,
     d.date_key,
     s.certification_name,
     s.certification_cost
-
 FROM {{ ref('stg_employee_certifications') }} AS s 
-INNER JOIN {{ ref('oliver_dim_employee') }} AS e 
-    ON s.employee_id = e.employee_key 
-INNER JOIN {{ ref('oliver_dim_date') }} AS d
-    ON s.certification_completion_id = d.date_key
+LEFT JOIN {{ ref('oliver_dim_employee') }} AS e 
+    ON CAST(s.employee_id AS STRING) = CAST(e.employee_key AS STRING)
+LEFT JOIN {{ ref('oliver_dim_date') }} AS d
+    ON CAST(s.certification_awarded_date AS DATE) = CAST(d.date_day AS DATE)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
